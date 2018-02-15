9b66c396a996bb78b1994144e90dc7f8dcf9611f
kotpsot <zygmunt.justyna@gmail.com>
2018-02-15 20:54:49 +0000

    fix view links test

--- spec/features/see_list_bookmarks_spec.rb
+++ spec/features/see_list_bookmarks_spec.rb
@@ -1,6 +1,6 @@
 feature 'viewing bookmarks' do
   scenario 'user can view all bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content("Makers Academy", "Google", "Facebook")
+    expect(page).to have_content("Makers Academy")
   end
 end
